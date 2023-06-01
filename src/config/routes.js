6c29831fe4ec51661fbfab170763d8cfd0157b2c
Daniel Kamenov <sorostmcoc@gmail.com>
2023-06-01 20:24:08 +0300

    added login and register pages

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const homeController = require('../controllers/homeController');
 const cubeController = require('../controllers/cubeController');
 const accessoryController = require('../controllers/accessoryController');
+const authController = require('../controllers/authController');
 
 // TODO: Require Controllers...
 
@@ -9,6 +10,7 @@ module.exports = (app) => {
     app.use(homeController);
     app.use('/cubes',cubeController);
     app.use('/accessory', accessoryController);
+    app.use('/',authController);
     app.get('*', (req, res) => {
         res.redirect('/404');
     });
