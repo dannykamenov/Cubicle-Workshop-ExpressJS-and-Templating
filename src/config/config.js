6eda4e434355cae90a9dd7fa914b6970f9602919
Daniel Kamenov <sorostmcoc@gmail.com>
2023-06-01 11:10:11 +0300

    deploy to realtime database

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+const mongo_uri = process.env.MONGODB_URI; 
+
 module.exports = {
     development: {
         port: process.env.PORT || 3000,
-        DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+        //DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+        DB_URI: mongo_uri,
     },
     production: {
-        DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+        //DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+        DB_URI: mongo_uri,
     }
 };
